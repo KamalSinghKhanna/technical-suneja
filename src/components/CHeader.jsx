591a8a9267650d683ffba9e8a77911d7e155a881
KamalSinghKhanna <kamalsinghkhanna2706@gmail.com>
2022-07-26 17:47:47 +0530

    just checking

---
+++ src/components/CHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class CHeader extends React.Component{
+    render(){
+        return <h2>Hello kamal</h2>
+    }
+}
+
+export default CHeader;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+export const Header = () => {
+
+  return <h2>Hello, functional components</h2>
+}
+
+// export default Header;
